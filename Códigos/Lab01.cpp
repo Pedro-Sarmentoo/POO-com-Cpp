2cf50edc276ca2b68715b414c2e5d502b2d4e1e0
Phsarmento <pedrohenriquesarmento@gmail.com>
2024-04-07 23:22:15 -0300

    Lab01 - Completo
    Implementação do código da conta bancaria feito e funcional

---
+++ Códigos/Lab01.cpp
@@ -0,0 +1,54 @@
+#include <strings.h>
+#include <iostream>
+
+class contaBancaria{
+
+    private:
+        std::string nomeCliente;
+        int numConta;
+        float saldoConta;
+
+    public:
+    //Prototipos das funções da classe contaBancaria
+        void exibir();
+        void depositar( float saldoConta);
+        void criarConta(std::string nomeCliente, int numConta, float saldoConta);
+        void sacar( float saldoConta);
+
+};
+
+void contaBancaria::criarConta(std::string nome, int num, float saldo){
+    nomeCliente = nome;
+    numConta = num;
+    saldoConta = saldo;
+
+    std::cout << "A conta foi criada" << std::endl;
+}
+//const std::string nome, const int num, const float saldo
+void contaBancaria::exibir(){std::cout << "Nome: " << nomeCliente << " Numero da conta: " << numConta << " Saldo: " << saldoConta << std::endl;}//Assim que printa na tela
+
+void contaBancaria::depositar(float valor){
+    saldoConta += valor;
+    std::cout << "O valor foi depositado" << std::endl;
+}
+
+void contaBancaria::sacar( float valor){
+    if(valor > saldoConta){std::cout << "Nao tem esse valor disponivel na sua conta!" << std::endl;}
+    else{
+        saldoConta -= valor;
+        std::cout << "O seu saque esta disponivel" << std::endl;
+    }
+}
+
+int main(){
+
+    contaBancaria pedro;
+    
+    pedro.criarConta("Pedro Henrique",1000,1000.50);
+    pedro.exibir();
+    pedro.sacar(100);
+    pedro.exibir();
+
+    return 0;
+}
+
